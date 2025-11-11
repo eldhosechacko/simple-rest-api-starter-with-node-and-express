0069a781c8c7fa25705563a45a5a88808656ed30
eldhosechacko <eldhosechacko@gmail.com>
2025-11-11 12:08:36 +0530

    updated

--- src/controllers/welcomeController.js
+++ src/controllers/welcomeController.js
@@ -1,3 +1,3 @@
 exports.getWelcome = (req, res) => {
-  res.json({ message: 'Welcome to the Simple REST API built with Node and Express!' });
+  res.json({ message: 'Welcome to the Simple REST API built with Node and Express!(Udated)' });
 };
